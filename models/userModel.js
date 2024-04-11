7c3b853ecd9ff22f5693a7fdb75e9328effa501f
Kelvyn <kelvynbigie90@gmail.com>
2024-04-12 02:00:21 +0700

    Revert "set username unique"
    This reverts commit 5b1db7f9b8934e604fa4bffc4a4fe1a5e1cd812b.

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const UserData = new mongoose.Schema({
-  name: { type: String, required: true, unique:true },
+  name: { type: String, required: true },
   password:{type :String ,required:true},
   isAdmin:{type :Boolean ,required:true, default:false},
 });
